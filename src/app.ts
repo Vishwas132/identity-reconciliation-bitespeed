1beccd1a7a2b0fb9c02df097be712fbc6c55b9b1
Vishwas Lekhwar <64309462+Vishwas132@users.noreply.github.com>
2024-08-07 18:35:46 +0530

    Add defaut route for server health check

--- src/app.ts
+++ src/app.ts
@@ -11,3 +11,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 app.use('/', contactController);
+
+// Health check
+app.get('/', (req, res) => {
+  res.send('Working!');
+});
